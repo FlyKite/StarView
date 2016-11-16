5b1b1f94709274b120d1f03b340edf1e512bd45e
FlyKite <DogeFlyKite@gmail.com>
2016-11-16 12:54:52 +0800

    Add a method to get points of star.

--- StarView/StarView.h
+++ StarView/StarView.h
@@ -9,9 +9,41 @@
 #import <UIKit/UIKit.h>
 
 @interface StarView : UIView
-@property (assign, nonatomic) BOOL enabled;
+
+@property (assign, nonatomic) BOOL enabled; // 默认为YES
+
+@property (assign, nonatomic) CGFloat lineWidth;    // 默认为1
+@property (strong, nonatomic) UIColor *strokeColor; // 默认为黑色
+
+/**
+ * 通过从传入两个点获取一个星形的UIBezierPath对象
+ *
+ * @param point1 起始点
+ * @param point2 第二个点
+ * @return 星形的UIBezierPath对象
+ */
 - (UIBezierPath *)starPathWithPoint1:(CGPoint)point1 andPoint2:(CGPoint)point2;
+
+/**
+ * 通过从传入两个点获取五角星形的五个顶点
+ *
+ * @param point1 起始点
+ * @param point2 第二个点
+ * @return 五角星形的五个顶点数组，数组中元素为NSValue存储的CGPoint（包括传入的两个点）
+ */
+- (NSArray<NSValue *> *)pointsOfStarVertexWithPoint1:(CGPoint)point1 andPoint2:(CGPoint)point2;
+
+/** TODO:目前返回结果与pointsOfStarVertexWithPoint1:andPoint2:方法相同
+ * 通过从传入两个点获取五角星形的十个点（五个定点与五个内凹的点）
+ *
+ * @param point1 起始点
+ * @param point2 第二个点
+ * @return 五角星形的十个点，数组中元素为NSValue存储的CGPoint（包括传入的两个点）
+ */
+- (NSArray<NSValue *> *)pointsOfStarWithPoint1:(CGPoint)point1 andPoint2:(CGPoint)point2;
+
+/**
+ * 清除当前view中所画出的所有五角星
+ */
 - (void)clear;
-@property (assign, nonatomic) CGFloat lineWidth;
-@property (strong, nonatomic) UIColor *strokeColor;
 @end
--- StarView/StarView.m
+++ StarView/StarView.m
@@ -26,6 +26,21 @@
 
 - (UIBezierPath *)starPathWithPoint1:(CGPoint)point1 andPoint2:(CGPoint)point2 {
     
+    NSArray *points = [self pointsOfStarVertexWithPoint1:point1 andPoint2:point2];
+    
+    UIBezierPath *path = [UIBezierPath bezierPath];
+    [path moveToPoint:[points[0] CGPointValue]];
+    [path addLineToPoint:[points[1] CGPointValue]];
+    [path addLineToPoint:[points[2] CGPointValue]];
+    [path addLineToPoint:[points[3] CGPointValue]];
+    [path addLineToPoint:[points[4] CGPointValue]];
+    [path addLineToPoint:[points[0] CGPointValue]];
+
+    return path;
+}
+
+- (NSArray<NSValue *> *)pointsOfStarVertexWithPoint1:(CGPoint)point1 andPoint2:(CGPoint)point2 {
+    
     CGFloat lineLength = sqrt(pow(point1.x - point2.x, 2) + pow(point1.y - point2.y, 2));
     CGPoint line1Center = CGPointMake((point1.x + point2.x) / 2, (point1.y + point2.y) / 2);
     CGFloat radius = lineLength / 2 / cos(M_PI / 10);
@@ -51,15 +66,51 @@
     CGFloat angle4 = atan(point45LineK);
     CGPoint point5 = CGPointMake(point4.x - cos(angle4) * lineLength * t, point4.y - sin(angle4) * lineLength * t);
     
-    UIBezierPath *path = [UIBezierPath bezierPath];
-    [path moveToPoint:point1];
-    [path addLineToPoint:point2];
-    [path addLineToPoint:point3];
-    [path addLineToPoint:point4];
-    [path addLineToPoint:point5];
-    [path addLineToPoint:point1];
+    NSMutableArray *points = [NSMutableArray arrayWithCapacity:5];
+    [points addObject:[NSValue valueWithCGPoint:point1]];
+    [points addObject:[NSValue valueWithCGPoint:point2]];
+    [points addObject:[NSValue valueWithCGPoint:point3]];
+    [points addObject:[NSValue valueWithCGPoint:point4]];
+    [points addObject:[NSValue valueWithCGPoint:point5]];
+    
+    return [points copy];
+}
 
-    return path;
+- (NSArray<NSValue *> *)pointsOfStarWithPoint1:(CGPoint)point1 andPoint2:(CGPoint)point2 {
+    
+    CGFloat lineLength = sqrt(pow(point1.x - point2.x, 2) + pow(point1.y - point2.y, 2));
+    CGPoint line1Center = CGPointMake((point1.x + point2.x) / 2, (point1.y + point2.y) / 2);
+    CGFloat radius = lineLength / 2 / cos(M_PI / 10);
+    
+    CGFloat line1k = (point2.y - point1.y) / (point2.x - point1.x);
+    CGFloat k = -1 / line1k;
+    CGFloat angle = atan(k);
+    
+    CGFloat t = point2.y < point1.y ? -1 : 1;
+    CGFloat tLength = sin(M_PI / 10) * radius;
+    CGPoint center = CGPointMake(line1Center.x - cos(angle) * tLength * t, line1Center.y - sin(angle) * tLength * t);
+    CGPoint point4 = CGPointMake(center.x + cos(angle) * radius * t, center.y + sin(angle) * radius * t);
+    
+    t = center.y < point1.y ? -1 : 1;
+    CGFloat point1CenterLineK = (center.y - point1.y) / (center.x - point1.x);
+    CGFloat point34LineK = -1 / point1CenterLineK;
+    CGFloat angle2 = atan(point34LineK);
+    CGPoint point3 = CGPointMake(point4.x - cos(angle2) * lineLength * t, point4.y - sin(angle2) * lineLength * t);
+    
+    t = point2.y < center.y ? -1 : 1;
+    CGFloat point2CenterLineK = (center.y - point2.y) / (center.x - point2.x);
+    CGFloat point45LineK = -1 / point2CenterLineK;
+    CGFloat angle4 = atan(point45LineK);
+    CGPoint point5 = CGPointMake(point4.x - cos(angle4) * lineLength * t, point4.y - sin(angle4) * lineLength * t);
+    
+    NSMutableArray *points = [NSMutableArray arrayWithCapacity:5];
+    [points addObject:[NSValue valueWithCGPoint:point1]];
+    [points addObject:[NSValue valueWithCGPoint:point2]];
+    [points addObject:[NSValue valueWithCGPoint:point3]];
+    [points addObject:[NSValue valueWithCGPoint:point4]];
+    [points addObject:[NSValue valueWithCGPoint:point5]];
+    
+    return [points copy];
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
